063dda0b20499d7adc9282efcf61fbfe9db9cf10
Hrishikesh Bhandari <rishi2001mahesh@gmail.com>
2022-04-07 12:34:18 +0530

    Create Code.ino

--- code/Code.ino
+++ code/Code.ino
@@ -2,6 +2,8 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <Firebase_ESP_Client.h>
+#include "OneWire.h"
+#include "DallasTemperature.h"
 
 //Provide the token generation process info.
 #include <addons/TokenHelper.h>
@@ -37,10 +39,23 @@ unsigned long count = 0;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org",19800);
 
+#define SensorPin 5            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 41.02740741 
+#define ArrayLenth  40    
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
 
-String stimer;
+String stimer, stimer1;
 String Str[]={"00:00","00:00","00:00"};
 int i,feednow=0;
+
+#define ONE_WIRE_BUS 18
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float tempAvg = 0.0;
+unsigned long starttime=0, nowtime=0;
+const unsigned long period = 60000;
 
 void setup() {
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
@@ -105,31 +120,75 @@ void setup() {
   Firebase.reconnectWiFi(true);
 
   Firebase.setDoubleDigits(5);
+  pinMode(4, OUTPUT);
+  sensors.begin();
+  starttime = millis();
 }
 
 void loop() {
-  if(Firebase.ready()){
-    Serial.printf("Get string... %s\n", Firebase.RTDB.getInt(&fbdo, F("/feednow")) ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
+//  if(Firebase.ready()){
+    nowtime = millis();
+    if(nowtime - starttime > period){
+       tempAvg = tempCal();
+       static unsigned long samplingTime = millis();
+        static unsigned long printTime = millis();
+        static float pHValue, voltage;
+  
+        pHArray[pHArrayIndex++] = analogRead(SensorPin);
+        if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+          voltage = avergearray(pHArray, ArrayLenth) * 3.3 / 4096;
+          pHValue = -19.18518519 * voltage + Offset;
+        timeClient.update();
+        unsigned long epochTime = timeClient.getEpochTime();
+        Serial.printf("Set ph... %s\n", Firebase.RTDB.setFloat(&fbdo, F("dev1/ph"), pHValue) ? "ok" : fbdo.errorReason().c_str());
+        delay(5000);
+        Serial.printf("Set temp... %s\n", Firebase.RTDB.setFloat(&fbdo, F("dev1/temp"), tempAvg) ? "ok" : fbdo.errorReason().c_str());
+        delay(5000);
+        Serial.printf("Set timestamp... %s\n", Firebase.RTDB.setFloat(&fbdo, F("dev1/timestamp"), epochTime) ? "ok" : fbdo.errorReason().c_str());
+        
+      starttime = nowtime;
+    }
+    Serial.printf("Get string... %s\n", Firebase.RTDB.getInt(&fbdo, F("dev1/feednow")) ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
     feednow = fbdo.to<int>();
+    Serial.printf("Get string... %s\n", Firebase.RTDB.getInt(&fbdo, F("dev1/delay")) ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
+    int delay1 = fbdo.to<int>();
     if (feednow==1){
+        
+       digitalWrite(4, HIGH);
+        delay(delay1);
+        digitalWrite(4, LOW);
 //      servo.writeMicroseconds(1000); // rotate clockwise
 //      delay(700); // allow to rotate for n micro seconds, you can change this to your need
 //      servo.writeMicroseconds(1500); // stop rotation
       feednow = 0;
-      Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/feednow"), feednow) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("dev1/feednow"), feednow) ? "ok" : fbdo.errorReason().c_str());
       Serial.println("Fed");
     }
     
     else{ // Scheduling feed
-      String path = "timers/timer"+String(i);
-      Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, F("timers/timer1")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
+      String path = "dev1/timers/timer"+String(i);
+      Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, F("dev1/timers/timer0")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
       stimer = fbdo.to<String>();
-      Str[i]= stimer.substring(9,14);
+      Str[0]= stimer.substring(9,14);
     timeClient.update();
-    String currentTime = String(timeClient.getHours())+":"+String(timeClient.getMinutes());
-    if (Str[0]==currentTime)
-    {
-//      servo.writeMicroseconds(1000); // rotate clockwise
+    char dateBuffer[12];
+    sprintf(dateBuffer,"%02u:%02u",timeClient.getHours(),timeClient.getMinutes());
+    Serial.println(Str[0].compareTo(dateBuffer));
+
+
+    Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, F("dev1/timers/timer1")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
+      stimer1 = fbdo.to<String>();
+      Str[1]= stimer1.substring(9,14);
+    timeClient.update();
+    Serial.println(Str[1].compareTo(dateBuffer));
+
+    
+    if (Str[0].equals(dateBuffer) || Str[1].equals(dateBuffer)){
+      Serial.println("Entered");
+      digitalWrite(4, HIGH);
+        delay(delay1);
+        digitalWrite(4, LOW);
+        //      servo.writeMicroseconds(1000); // rotate clockwise
 //      delay(700); // allow to rotate for n micro seconds, you can change this to your need
 //      servo.writeMicroseconds(1500); // stop rotation
       Serial.println("Success");
@@ -137,5 +196,53 @@ void loop() {
     }
   }
   Str[0]="00:00";
+  Str[1]="00:00";
+//  }
+}
+
+float tempCal(){
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  return temperatureC;
+}
+
+
+double avergearray(int* arr, int number) {
+  int i;
+  int max, min;
+  double avg;
+  long amount = 0;
+  if (number <= 0) {
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
   }
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
+    }
+    avg = amount / number;
+    return avg;
+  } else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
+    }
+    else {
+      min = arr[1]; max = arr[0];
+    }
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min;      //arr<min
+        min = arr[i];
+      } else {
+        if (arr[i] > max) {
+          amount += max;  //arr>max
+          max = arr[i];
+        } else {
+          amount += arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount / (number - 2);
+  }//if
+  return avg;
 }
